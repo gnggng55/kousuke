03a7a2dcd6bf253aeb30787bb5fc22b1cbbc3802
gnggng55 <kuk.kwge@gmail.com>
2021-11-19 15:41:06 +0900

    モデル・ビュー・コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "properties#index"
-  resources :properties, only: [:index, :new, :create]
+  resources :properties, only: [:index, :new, :create] do
+    resources :rooms, only: :index
+  end
   resources :items, only: [:index, :new, :create]
 end
