35dd98a270ca08756d1864a155951e61fd8907d8
gnggng55 <kuk.kwge@gmail.com>
2021-11-19 15:59:01 +0900

    登録画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "properties#index"
-  resources :properties, only: [:index, :new, :create] do
-    resources :rooms, only: :index
-  end
+  resources :properties, only: [:index, :new, :create]
+  resources :rooms, only: [:index, :new]
   resources :items, only: [:index, :new, :create]
 end
