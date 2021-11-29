66bc26faabe3e491cd87e1fe41219b102a489597
gnggng55 <kuk.kwge@gmail.com>
2021-11-30 05:25:11 +0900

    商品検索実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :gas
   
+
+  def self.search(search)
+    if search != ""
+      Item.where('product_number LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
+
 end
